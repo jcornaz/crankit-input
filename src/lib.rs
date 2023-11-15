5d6c28fad10a0c8e5c19f62523a84feb5682eb72
Jonathan Cornaz <jcornaz@users.noreply.github.com>
2023-11-15 21:47:11 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+#![deny(future_incompatible, nonstandard_style, private_in_public)]
+#![warn(clippy::pedantic, clippy::exhaustive_enums, clippy::exhaustive_structs)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
