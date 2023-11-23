cbf5844a189cf314ea901ece11a6d46bf520ee10
Jonathan Cornaz <jcornaz@users.noreply.github.com>
2023-11-23 12:58:07 +0100

    update rust docs of root module

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@
 //!
 //! ## Feature flags
 //!
-//! * `playdate-sys-v02` (default): provides implementations of the input source traits for the type `ffi::playdate_sys` and `ffi::PlaydateAPI` of the crate [`playdate-sys`](https://docs.rs/playdate-sys/0.2) (version `0.2`)
+//! * `playdate-sys-v02`: provides implementations of the input source traits for the type `ffi::playdate_sys` and `ffi::PlaydateAPI` of the crate [`playdate-sys`](https://docs.rs/playdate-sys/0.2) (version `0.2`)
 
 mod button;
 mod interop;
